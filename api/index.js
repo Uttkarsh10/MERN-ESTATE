39f733381c585ed3e8815d077563a23acccbb246
Uttkarsh <uttkarshsaini10@gmail.com>
2024-01-14 02:08:23 +0530

    Connected with database

--- api/index.js
+++ api/index.js
@@ -6,9 +6,23 @@
 //     console.log("Server is running or port 3000");
 // });
 
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
+    console.log("Connected to mongoDB");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
