0cb9870959af92b9264680d01e5722be980e5ea3
Uttkarsh <uttkarshsaini10@gmail.com>
2024-01-14 23:04:36 +0530

    SignUp

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -34,3 +35,4 @@ app.listen(port, () => {
 })
 
 app.use("/api/user", userRouter);
+app.use("api/auth", authRouter);
